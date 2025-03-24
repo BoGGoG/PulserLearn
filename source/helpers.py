5b34b38d5900cd4da00e6043d71ffe3ba5f38d95
Marco Knipfer <marco.knipfer@me.com>
2025-03-24 16:42:05 +0100

    Clique

---
+++ source/helpers.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def matprint(mat, fmt="g"):
+    """https://gist.github.com/braingineer/d801735dac07ff3ac4d746e1f218ab75"""
+    col_maxes = [max([len(("{:"+fmt+"}").format(x)) for x in col]) for col in mat.T]
+    for x in mat:
+        for i, y in enumerate(x):
+            print(("{:"+str(col_maxes[i])+fmt+"}").format(y), end="  ")
+        print("")
